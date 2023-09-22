465a0b85f614503c7606424c5501cbb6018e1d0d
EchoEye <femiobajuluwa@gmail.com>
2023-09-22 11:21:05 +0100

    updated key map

--- src/pages/fleets.tsx
+++ src/pages/fleets.tsx
@@ -50,8 +50,8 @@ export default function Home() {
 
           <div>
             <TabsList className="bg-white h-auto py-0 px-0">
-              {tabs.map((item, index) => <div className={``}>
-                <TabsTrigger key={item.value} className="mx-0 w-36 py-4 data-[state=active]:bg-[#F9F9FE] text-gray-500 rounded-none data-[state=active]:text-primary data-[state=active]:border-b-2 border-b-primary" value={item.value}>{item.name}</TabsTrigger>
+              {tabs.map((item) => <div className={``} key={item.value}>
+                <TabsTrigger className="mx-0 w-36 py-4 data-[state=active]:bg-[#F9F9FE] text-gray-500 rounded-none data-[state=active]:text-primary data-[state=active]:border-b-2 border-b-primary" value={item.value}>{item.name}</TabsTrigger>
               </div>)}
             </TabsList>
           </div>
