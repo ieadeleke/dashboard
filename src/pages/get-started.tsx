e346fe99a0eeb4c83605e25f76eff6e6d459000e
EchoEye <femiobajuluwa@gmail.com>
2023-10-06 12:15:04 +0100

    added get started page

--- src/pages/get-started.tsx
+++ src/pages/get-started.tsx
@@ -1,26 +1,44 @@
+
 import AuthLayout from "@/components/layout/AuthLayout";
 import LogoIcon from '@/assets/icons/ic_logo.svg'
 import { TextField } from "@/components/input/InputText";
 import { IconButton } from "@/components/buttons/IconButton";
 import { EyeIcon } from "lucide-react";
-import { CheckBox } from "@/components/buttons/CheckBox";
 import Button from "@/components/buttons";
 import Link from "next/link";
 
-export default function LoginPage() {
+export default function GetStartedPage() {
     return <AuthLayout>
-        <div className="flex flex-col  min-h-screen items-center justify-center py-8">
+        <div className="relative flex flex-col  min-h-screen items-center justify-center py-8">
+
+            <Link href="/login">
+                <Button variant="outlined" className="absolute py-3 right-2 top-4">Already Have an Account</Button>
+                </Link>
 
             <div className="flex flex-col gap-8 w-full px-2 md:w-[400px]">
                 <div className="flex flex-col gap-4">
                     <LogoIcon className="self-center" />
                     <div className="flex flex-col gap-2 text-center">
-                        <h1 className="font-bold text-2xl md:text-3xl text-primary">Log In to Admin</h1>
+                        <h1 className="font-bold text-2xl md:text-3xl text-primary">Get Started</h1>
                         <p className="text-[#666666]">Control Center</p>
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                        <p className="font-medium text-[#3F3F3F]">First Name</p>
+                        <TextField.Container className="bg-white rounded:lg">
+                            <TextField.Input />
+                        </TextField.Container>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <p className="font-medium text-[#3F3F3F]">Last Name</p>
+                        <TextField.Container className="bg-white rounded:lg">
+                            <TextField.Input />
+                        </TextField.Container>
+                    </div>
+
                     <div className="flex flex-col gap-2">
                         <p className="font-medium text-[#3F3F3F]">Email</p>
                         <TextField.Container className="bg-white rounded:lg">
@@ -40,17 +58,8 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <div className="flex items-center gap-4">
-                    <CheckBox />
-                    <p className="font-semibold text-[#212121]">Remember Me</p>
-                </div>
-
                 <div className="flex flex-col gap-4">
-                    <Button className="py-4" variant="contained">Login</Button>
-
-                    <Link href='/reset-password' className="self-center">
-                        <Button className="py-4" variant="text">Can't remember your password?</Button>
-                    </Link>
+                    <Button className="py-4" variant="contained">Sign Up</Button>
                 </div>
 
             </div>
