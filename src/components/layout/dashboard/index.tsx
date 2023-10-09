c4c2a6427c29604014c428824d43870fd593a01b
EchoEye <femiobajuluwa@gmail.com>
2023-10-09 12:46:35 +0100

    added max height to menu side bar

--- src/components/layout/dashboard/index.tsx
+++ src/components/layout/dashboard/index.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout(props: LayoutProps) {
 
     return <div className="relative max-w-[1440px] bg-r flex mx-auto">
         <div className="hidden h-screen mx-auto sticky top-0 bottom-0 w-auto md:flex lg:w-[20%]">
-            <div className="w-full h-screen  py-8 px-2 overflow-y-hidden">
+            <div className="w-full h-screen max-h-[1000px] py-8 px-2 overflow-y-hidden">
                 <MenuSideBar />
             </div>
         </div>
