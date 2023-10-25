a59ae193268fee25f9631b3927c12ac9922f9d7e
EchoEye <femiobajuluwa@gmail.com>
2023-10-25 10:58:27 +0100

    added base ws url

--- src/utils/constants/strings.ts
+++ src/utils/constants/strings.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_PROFILE_URL = "https://picsum.photos/200"
 export const BASE_URL = "https://laswa.damdamtechnology.com"
+export const BASE_WEBSOCKET_URL="ws://laswa.damdamtechnology.com"
