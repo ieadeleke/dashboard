8de6cbd9e52d9049fb18742f6258edd73c06eb23
EchoEye <femiobajuluwa@gmail.com>
2023-10-30 16:27:44 +0100

    fix build error

--- src/components/input/RegularTextInput.tsx
+++ src/components/input/RegularTextInput.tsx
@@ -7,3 +7,5 @@ export type InputProps = InputHTMLAttributes<HTMLInputElement>
 export const RegularTextInput =forwardRef<HTMLInputElement, InputProps>(({ className, ...props }: InputProps, ref) => {
     return <Input ref={ref} className={cn("w-full px-3 py-3", className)} {...props} />
 })
+
+RegularTextInput.displayName = "RegularTextInput"
