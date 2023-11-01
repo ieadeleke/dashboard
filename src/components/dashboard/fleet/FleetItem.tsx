953c26b348d6231ef87f4bef36d04c2dd201eab2
EchoEye <femiobajuluwa@gmail.com>
2023-11-01 15:52:18 +0100

    added mock data

--- src/components/dashboard/fleet/FleetItem.tsx
+++ src/components/dashboard/fleet/FleetItem.tsx
@@ -1,19 +1,26 @@
 import { Avatar } from "@/components/image/Avatar"
 import { DEFAULT_PROFILE_URL } from "@/utils/constants/strings"
+import { faker } from '@faker-js/faker'
+
+const image = faker.image.dataUri()
+const name = faker.airline.airline().name
+const username = faker.name.zodiacSign()
+const count = faker.number.int({ min: 54, max: 1000 })
 
 export const FleetItem = () => {
+
     return <div className="flex flex-col gap-4">
-        <img src="" className="bg-red-200 rounded-lg h-[400px]" />
+        <img src={image} className="bg-grey-200 object-cover object-center rounded-lg h-[400px]" />
 
         <div className="flex flex-col gap-3">
             <div className="flex justify-between items-center">
-                <h1 className="font-bold text-text-color text-xl md:text-2xl">Boat Name</h1>
-                <p className="text-gray-600">216 Passengers</p>
+                <h1 className="font-bold text-text-color text-xl md:text-2xl">{name}</h1>
+                <p className="text-gray-600">{count} Passengers</p>
             </div>
 
             <div className="flex items-center gap-2">
                 <Avatar src={DEFAULT_PROFILE_URL} className="w-8 h-8" />
-                <p>@m_alisson</p>
+                <p>@{username}</p>
             </div>
 
             <p className="text-text-color font-bold">Active</p>
