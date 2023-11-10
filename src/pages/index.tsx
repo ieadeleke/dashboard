b0ac4f1f560b364d4eb2463db6184597faeda625
EchoEye <femiobajuluwa@gmail.com>
2023-11-10 17:55:49 +0100

    redirect on no token with 401

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,26 @@ import { ActivitiesService } from "@/utils/services/activity";
 import { GetDashboardStatistics } from "@/models/activities/ActivitiesResponse";
 import { errorHandler } from "@/utils/errorHandler";
 import Unauthorized from "@/components/states/UnAuthorized";
+import AuthToken from '@/utils/AuthToken'
+import { useEffect } from "react";
+import roundToNearestMinutes from "date-fns/esm/fp/roundToNearestMinutes/index.js";
+import { useRouter } from "next/router";
 
 type HomeProps = {
   data: GetDashboardStatistics,
-  unauthorized?: boolean
+  unauthorized?: boolean,
+  redirect?: boolean
 }
 
 export default function Home(props: HomeProps) {
   const { data } = props
+  const router = useRouter()
+
+  // useEffect(() => {
+  //   if(props.redirect){
+  //     router
+  //   }
+  // })
 
   if (props.unauthorized) {
     return <DashboardLayout>
@@ -49,10 +61,20 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     }
   } catch (error) {
     const parsedError = errorHandler(error)
-    if (parsedError.status == 401) {
+    const token = AuthToken().retrieveToken(req)
+    if (parsedError.status == 401 && token) {
       return {
         props: {
           unauthorized: true
+        }
+      }
+    }else if (parsedError.status == 401) {
+      return {
+        redirect: {
+          destination: '/login'
+        },
+        props: {
+          
         }
       }
     }
