034c86470003e55125f8971a067b7afe7bfbcbff
EchoEye <femiobajuluwa@gmail.com>
2023-12-20 10:52:30 +0000

    removed unused imports

--- src/components/page_components/track-package/PackageStatus.tsx
+++ src/components/page_components/track-package/PackageStatus.tsx
@@ -1,9 +1,5 @@
-import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import StepContent from "@mui/material/StepContent";
 import { CheckIcon } from "lucide-react";
 
