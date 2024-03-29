92cdad47fba786d97d33f9fc4b1d49ede5d5c737
echoeyecodes <femiobajuluwa@gmail.com>
2024-03-29 12:12:36 +0100

    adjusted styles

--- src/components/layout/dashboard/index.tsx
+++ src/components/layout/dashboard/index.tsx
@@ -11,7 +11,7 @@ type LayoutProps = {
 
 export default function DashboardLayout(props: LayoutProps) {
   return (
-    <div className="relative max-w-[1440px] flex mx-auto">
+    <div className="relative max-w-[2000px] flex mx-auto">
       <div className="hidden h-screen mx-auto sticky top-0 bottom-0 w-auto md:flex lg:w-[20%]">
         <div className="w-full h-screen max-h-[1000px] overflow-y-hidden">
           <MenuSideBar />
