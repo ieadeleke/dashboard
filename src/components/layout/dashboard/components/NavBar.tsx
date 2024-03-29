98e2f708155090cc9fae1714c8daea9a09602036
echoeyecodes <femiobajuluwa@gmail.com>
2024-03-29 11:43:25 +0100

    added page guard

--- src/components/layout/dashboard/components/NavBar.tsx
+++ src/components/layout/dashboard/components/NavBar.tsx
@@ -1,24 +1,25 @@
-import { IconButton } from "@/components/buttons/IconButton";
 import { Avatar } from "@/components/image/Avatar";
-import BellIcon from '@/assets/icons/ic_notification_bell.svg'
-import MailIcon from '@/assets/icons/ic_mail.svg'
+import { useContext } from "react";
+import UserContext from "@/context/UserContext";
 
 export const NavBar = () => {
+  const { user } = useContext(UserContext)
+
   return (
     <div className="flex justify-end h-20 bg-white px-8">
       <div className="flex items-center gap-4">
-        <IconButton>
+        {/* <IconButton>
           <MailIcon className="w-7 h-7" />
         </IconButton>
 
         <IconButton>
           <BellIcon className="w-7 h-7" />
-        </IconButton>
+        </IconButton> */}
 
         <div className="flex items-center gap-2">
           <Avatar className="bg-gray-500 w-12 h-12" />
           <div>
-            <h2 className="font-bold text-sm">Ashley</h2>
+            <h2 className="font-bold text-sm">{user?.firstName} {user?.lastName}</h2>
             <p className="text-sm">User</p>
           </div>
         </div>
