47ff2997b4f73c9f9745d6dfaff18fec53828135
echoeyecodes <femiobajuluwa@gmail.com>
2024-04-03 13:59:33 +0100

    updated label

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -64,7 +64,7 @@ export default function Transactionss() {
                     </form>
                     <NetworkRequestContainer isLoading={isLoading} error={error} onRetry={fetchData}>
                         {(!isLoading && !error) && <TransactionTable
-                            name="Recent Transactions"
+                            name="All Transactions"
                             transactions={transactions}
                             isLoading={isLoading}
                             fetchData={fetchData}
