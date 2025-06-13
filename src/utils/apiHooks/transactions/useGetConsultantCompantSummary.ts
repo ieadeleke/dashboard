1749e0914be6d678c9fa0c485f3685d23184d3eb
ieadeleke <eadelekeife@yahoo.com>
2025-06-13 13:01:41 +0100

    landing page error fixed

--- src/utils/apiHooks/transactions/useGetConsultantCompantSummary.ts
+++ src/utils/apiHooks/transactions/useGetConsultantCompantSummary.ts
@@ -7,13 +7,13 @@ export const useDashboardInfoDownload = () => {
     const [data, setData] = useState<DashboardInfoResponseParams | null>(null)
     const { isLoading, error, execute } = useApi()
 
-    async function getSummaryDownloadInfo() {
-        setData(null)
-        const response = await execute(async () => await TransactionService().getConsultantCompanySummary());
-        console.log(response);
-        if (response) {
-            setData(response)
-        }
+    async function getSummaryDownloadInfo(params: any) {
+        // setData(null)
+        // const response = await execute(async () => await TransactionService().getConsultantCompanySummary());
+        // console.log(response);
+        // if (response) {
+        //     setData(response)
+        // }
     }
 
     return { isLoading, error, data, getSummaryDownloadInfo }
